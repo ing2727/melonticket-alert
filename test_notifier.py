47eca1d1f05b68ef51361da546c65c9343dde1fc
ing2727 <ing2727@gmail.com>
2025-06-08 18:06:13 +0900

    Update test_notifier.py

--- test_notifier.py
+++ test_notifier.py
@@ -4,16 +4,3 @@ from notifier import send_slack, send_email
 
 send_slack("슬랙 웹훅 테스트 메시지입니다 🎫")
 send_email("테스트 메일", "멜론 티켓 알림 시스템 이메일 테스트입니다.")
-
-
-# test_notifier.py
-
-from notifier import send_slack, send_email
-
-send_slack("티켓있어 🎫")
-send_email("테스트 메일", "멜론 티켓 알림 시스템 이메일 테스트입니다.")
-
-from notifier import send_slack, send_email
-
-send_slack("슬랙 웹훅 테스트 메시지입니다 🎫")
-send_email("테스트 메일", "멜론 티켓 알림 시스템 이메일 테스트입니다.")
